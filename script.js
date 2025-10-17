4f104fc94e6b742f2016e2101f288215b326b553
jzapdos <226jmanley@frhsd.com>
2025-10-17 08:32:24 -0400

    first

---
+++ script.js
@@ -0,0 +1,91 @@
+// const para = document.querySelector('body > h1');
+
+// console.log(para);
+
+// const paras = document.querySelectorAll('p');
+// const errors = document.querySelectorAll('.error');
+
+// console.log(errors);
+
+// const title = document.getElementById('page-title');
+// console.log(title);
+
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+// console.log(errors[0]);
+// errors.forEach(error => {
+//     console.log(error);
+// })
+
+// const paras = document.getElementsByTagName('p');
+// console.log(paras);
+// console.log(paras[1]);
+
+// const para = document.querySelector('p');
+
+// console.log(para.innerText);
+// para.innerText = 'ninjas are awesome';
+
+// const paras = document.querySelectorAll('p');
+
+// paras.forEach(para => {
+//     console.log(para.innerText);
+//     para.innerHTML += ' new text';
+// })
+
+// const content = document.querySelector('.content');
+
+// console.log(content.innerHTML);
+// content.innerHTML += '<h2>THIS IS A NEW H2</h2>';
+
+// const people = ['mario', 'luigi', 'yoshi'];
+
+// people.forEach(person => {
+//     content.innerHTML += `<p>${person}</p>`
+// });
+
+// const link = document.querySelector('a');
+
+// console.log(link.getAttribute('href'));
+// link.setAttribute('href', 'https://www.thenetninja.co.uk');
+// link.innerText = 'The Net Ninja Website';
+
+// const mssg = document.querySelector('p');
+
+// console.log(mssg.getAttribute('class'));
+// mssg.setAttribute('class', 'success');
+// mssg.setAttribute('style', 'color: green;');
+
+// const title = document.querySelector('h1');
+
+// title.setAttribute('style', 'margin: 50px;');
+
+// console.log(title.style);
+// console.log(title.style.color);
+
+// title.style.margin = '50px';
+// title.style.color = 'crimson';
+// title.style.fontSize = '60px';
+// title.style.margin = '';
+
+// const content = document.querySelector('p');
+
+// console.log(content.classList);
+// content.classList.add('error');
+// content.classList.remove('error');
+// content.classList.add('success');
+
+const paras = document.querySelectorAll('p');
+paras.forEach(para => {
+    if (para.textContent.includes('error')) {
+        para.classList.add('error');
+    }
+    if (para.textContent.includes('success')) {
+        para.classList.add('success');
+    }
+});
+
+const title = document.querySelector('.title');
+
+title.classList.toggle('test');
+title.classList.toggle('test');
